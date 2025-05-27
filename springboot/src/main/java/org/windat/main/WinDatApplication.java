a4f50ddb205a37ae68cb5864516b11d90ae38afb
Mykyta Prykhodko <dlagamekontrol@gmail.com>
2025-05-27 23:24:10 +0200

    Added monthly rewards feature

--- springboot/src/main/java/org/windat/main/WinDatApplication.java
+++ springboot/src/main/java/org/windat/main/WinDatApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.windat.*"})
 //@EnableJpaRepositories("org.windat.jpa.repository")
+// For monthly prizes
+@EnableScheduling
 public class WinDatApplication
 {
     public static void main(String[] args)
